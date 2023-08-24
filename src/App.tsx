8383a3ab00e531b2889dc2f079ca4e41c63d0608
Jakob Larsson <jakob.larsson@devies.se>
2023-08-24 11:29:12 +0200

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import type { Component } from 'solid-js';
+import { lazy } from 'solid-js';
+import { Routes, useRoutes } from '@solidjs/router';
 import Comp from './Comp';
 
+const routes = [ 
+  {
+    path: "/",
+    component: Comp,
+  },
+  {
+    path: "/about",
+    component: lazy(() => import("./pages/About.js")),
+  }
+];
+
 const App: Component = () => {
+  const Routes = useRoutes(routes);
+
   return (
     <>
-      <h1>Hello world!!!!</h1>
-      <Comp />
+      <h1>Trem</h1>
+      <Routes />
     </>
   );
 };
---
+++ src/pages/About.tsx
@@ -0,0 +1,8 @@
+export default () => {
+  return <>
+    <h2>About Page</h2>
+    <main>
+      <p>This is the app Trem. It is a poc using Solid.js</p>
+    </main>
+  </>;
+};
